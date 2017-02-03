fafd26f97798ed6ee63bdf46e9b1db6190025264
unknown <>
2017-02-03 17:53:28 +0800

    fix

--- pages/movies/movie-detail/movie-detail.js
+++ pages/movies/movie-detail/movie-detail.js
@@ -1,5 +1,5 @@
 var app = getApp();
-var util = require('../../../utils/utils.js');
+import { Movie } from 'class/Movie.js';
 
 Page({
   data: {
@@ -8,44 +8,13 @@ Page({
   onLoad: function (options) {
     var movieId = options.id;
     var url = app.globalData.doubanBase + "/v2/movie/subject/" + movieId;
-    util.http(url, this.processDoubanData);
+    var movie = new Movie(url);
+    movie.getMovieData((movie) => { //es6箭头函数中的this指代的是当前定义函数的环境，而不是调用对象的环境，所有不需要用that=this
+      this.setData({
+        movie: movie
+      })
+    })
 
-  },
-
-  processDoubanData: function (data) {
-    if (!data) {
-      return;
-    }
-    var director = {
-      avatar: "",
-      name: "",
-      id: ""
-    }
-    if (data.directors[0] != null) {
-      if (data.directors[0].avatars != null) {
-        director.avatar = data.directors[0].avatars.large
-
-      }
-      director.name = data.directors[0].name;
-      director.id = data.directors[0].id;
-    }
-    var movie = {
-      movieImg: data.images ? data.images.large : "",
-      country: data.countries[0],
-      title: data.title,
-      originalTitle: data.original_title,
-      wishCount: data.wish_count,
-      commentCount: data.comments_count,
-      year: data.year,
-      generes: data.genres.join("、"),
-      stars: util.convertToStarsArray(data.rating.stars),
-      score: data.rating.average,
-      director: director,
-      casts: util.convertToCastString(data.casts),
-      castsInfo: util.convertToCastInfos(data.casts),
-      summary: data.summary
-    }
-    this.setData({movie: movie});
   },
 
   ///查看图片
